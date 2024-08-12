4bd1b2a8b07fb99f1c77bd1a2ab4097827a8b9df
Ruslan Mirasov <info@mirasov.dev>
2024-08-12 14:23:23 +0200

    sdsdsd

--- src/js/scroll-animation.js
+++ src/js/scroll-animation.js
@@ -5,11 +5,6 @@ window.addEventListener('scroll', throttle(animate, 150));
 window.addEventListener('resize', throttle(animate, 150));
 
 export function animate() {
-  if (window.scrollY > 0) {
-    refs.header.classList.add('fix');
-  } else {
-    refs.header.classList.remove('fix');
-  }
   refs.amimateElements.forEach(element => {
     const windowHeight = window.innerHeight;
     const animationRepeat = element.dataset.repeat;
